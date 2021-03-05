1caacabbc5b9890a11bb54d210ddab4982145cd2
01raza <ra0074@mix.wvu.edu>
2021-03-04 23:16:13 -0500

    Raza Hypothesis Upload

---
+++ Hypothesis.r
@@ -0,0 +1,5 @@
+### Hypothesis : "American women have a wider pelvic inlet, wider outlet, and shallower anteroposterior outlet than Asian women."
+
+setwd('C:\\users\\Raza\\Desktop\\Evolution\\Project') 
+Data <- read.csv("pelvic_size_data.csv",stringsAsFactors=F)
+head(Data)
