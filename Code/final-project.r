5a799808943eda1e4c7d800a8fb1777a38baa982
01raza <ra0074@mix.wvu.edu>
2021-03-19 12:00:16 -0400

    project test

---
+++ Code/final-project.r
@@ -0,0 +1,4 @@
+setwd('C:\\Users\\Raza\\Desktop\\Evolution\\Project\\Data') 
+dir()
+data<- read.csv("brain.csv",stringsAsFactors=F)
+head(data)
--- Hypothesis.r
+++
@@ -1,5 +0,0 @@
-### Hypothesis : "American women have a wider pelvic inlet, wider outlet, and shallower anteroposterior outlet than Asian women."
-
-setwd('C:\\users\\Raza\\Desktop\\Evolution\\Project') 
-Data <- read.csv("pelvic_size_data.csv",stringsAsFactors=F)
-head(Data)
