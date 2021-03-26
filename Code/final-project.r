f5f29bcec9af2316bc2cc68f9570d0efdeef2948
01raza <ra0074@mix.wvu.edu>
2021-03-26 10:18:54 -0400

    Final Project

--- Code/final-project.r
+++ Code/final-project.r
@@ -1,6 +1,22 @@
 setwd('C:\\Users\\Raza\\Desktop\\Evolution\\Project\\Data') 
-dir()
+#dir()
 data<- read.csv("brain.csv",stringsAsFactors=F)
-head(data)
 data$brain.body.ratio<-(data$brain.mass/data$body.mass)
-head(data)
+rownames(data) <- gsub(" ", "_", data[,2])
+#head(data)
+
+
+library(phytools)
+Trees <- read.nexus("tree.nex")
+Phy <- Trees[[1]]
+
+Pruned <- data[Phy$tip.label,]
+x <- Pruned$brain.body.ratio
+names(x) <- rownames(Pruned)
+
+par(mfrow=c(1,2))
+contMap(Phy, x, fsize = c(.1,1))
+plotTree.wBars(Phy, x)
+
+# This will take forever
+phenogram(Phy, x, fsize=0.01)
