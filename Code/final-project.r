f78f642f429179cedf5c729cf1c1293d0716f667
01raza <ra0074@mix.wvu.edu>
2021-04-02 08:44:06 -0400

    BAMM and DR code

--- Code/final-project.r
+++ Code/final-project.r
@@ -14,9 +14,68 @@ Pruned <- data[Phy$tip.label,]
 x <- Pruned$brain.body.ratio
 names(x) <- rownames(Pruned)
 
+pdf("Phyl_tree", height = 5, width = 10)
 par(mfrow=c(1,2))
 contMap(Phy, x, fsize = c(.1,1))
 plotTree.wBars(Phy, x)
+dev.off()
 
 # This will take forever
 phenogram(Phy, x, fsize=0.01)
+
+
+### Analyze BAMM results
+library(BAMMtools)
+setwd('C:\\Users\\Raza\\Desktop\\bamm-2.5.0-Windows') 
+Tree <- read.tree("phy.tre")
+tree <- Tree[[1]]
+eData <- getEventData(tree, "mar31_event_data.txt", burnin=0.5)
+trait <- data[eData$tip.label, "brain.body.ratio"]
+names(trait) <- eData$tip.label
+plot(eData, spex="s") # set spex to e for extinction, netdiv for net diversification
+
+meanSpeciation <- eData$meanTipLambda
+names(meanSpeciation) <- tree$tip.label
+
+meanExtinction <- eData$meanTipMu
+names(meanExtinction) <- tree$tip.label
+
+plot(meanSpeciation, trait)
+
+## Maybe look into STRAPP
+strappBBR <- traitDependentBAMM(eData, trait, reps=100)
+
+
+### DR Statistic
+# DR metric / inverse equal splits
+# WRITTEN BY PASCAL TITLE
+DRstat <- function(tree) {
+  
+  spRate <- function(sp, tree) {
+    #get branch lengths from root to tip
+    edges <- vector()
+    daughterNode <- match(sp, tree$tip.label)
+    while (daughterNode != (length(tree$tip.label) + 1)) {
+      parentNode <- tree$edge[which(tree$edge[,2] == daughterNode), 1]
+      edges <- c(edges, tree$edge.length[which(tree$edge[,1] == parentNode & tree$edge[,2] == daughterNode)])
+      daughterNode <- parentNode
+    }
+    
+    res <- sum(sapply(1:length(edges), function(x) edges[x] * (1/(2 ^ (x-1)))))
+    res <- res ^ (-1)
+    
+    return(res)
+  }
+  
+  rates <- unlist(lapply(tree$tip.label, function(x) spRate(x, tree)))
+  names(rates) <- tree$tip.label
+  
+  return(rates)
+}
+
+DRrates <- DRstat(tree)
+plot(DRrates, trait)
+
+# this does NOT account for autoregression! (don't trust)
+cor.test(DRrates, trait, method="spear")
+
